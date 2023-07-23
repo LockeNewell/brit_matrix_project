cb70d35e02f4a928070b34228aedd64702c664da
Locke Newell <locke.newell@gmail.com>
2023-07-22 21:08:24 -0500

    fix the test
    fixed the test error I created and moves the hello world view as a sanity check

--- brit_matrix_app/tests.py
+++ brit_matrix_app/tests.py
@@ -6,4 +6,4 @@ class HelloWorldViewTests(TestCase):
         url = reverse('hello_world')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "bob")
+        self.assertContains(response, "Hello, World!")
