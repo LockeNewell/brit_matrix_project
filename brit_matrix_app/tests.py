8d0d1f113ee0fc684af7ed4edd24a860ece435f8
Locke Newell <locke.newell@gmail.com>
2023-07-22 21:11:16 -0500

    add test for bilingual reading view
    checking my template works as expected

--- brit_matrix_app/tests.py
+++ brit_matrix_app/tests.py
@@ -7,3 +7,10 @@ class HelloWorldViewTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Hello, World!")
+
+class BilingualReadingViewTests(TestCase):
+    def test_bilingual_reading_view(self):
+        url = reverse('bilingual_reading')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bilingual_reading.html')
