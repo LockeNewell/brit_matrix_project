e66af801cf78f1d8f3a1bae5ff1b875316a7a4f0
Locke Newell <locke.newell@gmail.com>
2023-07-22 21:31:49 -0500

    add bilingual_reading template
    added the primary page for this project
    
    I also added some more rules for my .gitignore

--- brit_matrix_app/urls.py
+++ brit_matrix_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello_world, name='hello_world'),
+    path('', views.bilingual_reading_view, name='bilingual_reading'),
+
+    path('hello/', views.hello_world, name='hello_world'),
 ]
--- brit_matrix_app/views.py
+++ brit_matrix_app/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
     return HttpResponse("Hello, World!")
+def bilingual_reading_view(request):
+    return render(request, 'bilingual_reading.html')
