eb2690de7ebf83fe5d46f100abf4d1e72e0c017c
Locke Newell <locke.newell@gmail.com>
2023-07-24 14:38:51 -0500

    wait no it changes again....
    the form changed so Language Proficiency are always visible
    Reading Connected Text would only show after Language Proficiency was not default
    Reading Words would only show if Reading Connected Text is not accurate
    Decoding would only show if Reading Words is not accurate
    
    submit would be disabled if all dropdowns are not the default value excluding Phonemic Awareness

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,12 @@
 // Get the form elements and the submit button
+const result = document.getElementById('result');
 const form = document.getElementById('myForm');
 const clearButton = document.getElementById('clearButton');
 const submitButton = document.getElementById('submitButton');
+const language_proficiency_panel = document.getElementById('language_proficiency_panel');
+const language_proficiency_L1 = document.getElementById('language_proficiency_L1');
+const language_proficiency_L2 = document.getElementById('language_proficiency_L2');
+const reading_connected_text_panel = document.getElementById('reading_connected_text_panel');
 const reading_connected_text_L1 = document.getElementById('reading_connected_text_L1');
 const reading_connected_text_L2 = document.getElementById('reading_connected_text_L2');
 const reading_words_panel = document.getElementById('reading_words_panel');
@@ -13,9 +18,6 @@ const decoding_L2 = document.getElementById('decoding_L2');
 const phonemic_awareness_panel = document.getElementById('phonemic_awareness_panel');
 const phonemic_awareness_L1 = document.getElementById('phonemic_awareness_L1');
 const phonemic_awareness_L2 = document.getElementById('phonemic_awareness_L2');
-const language_proficiency_panel = document.getElementById('language_proficiency_panel');
-const language_proficiency_L1 = document.getElementById('language_proficiency_L1');
-const language_proficiency_L2 = document.getElementById('language_proficiency_L2');
 
 
 // const dropdownL2 = document.getElementById('id_l2_choice');
@@ -26,133 +28,96 @@ function resetDropdowns() {
   dropdowns.forEach((dropdown) => {
     dropdown.value = 'default';
   });
+  reading_connected_text_panel.classList.add('d-none');
   reading_words_panel.classList.add('d-none');
   decoding_panel.classList.add('d-none');
-  phonemic_awareness_panel.classList.add('d-none');
-  language_proficiency_panel.classList.add('d-none');
 
   submitButton.disabled = true;
-}
-// Function to reset all reading words options to default
-function resetReadingWordsDropdowns() {
-  const dropdowns = form.querySelectorAll('[id$="reading_words_L1"]', '[id$="reading_words_L2"]');
-  dropdowns.forEach((dropdown) => {
-    dropdown.value = 'default';
-  });
-  resetDecodingDropdowns();
-}
-// Function to reset all reading words options to default
-function resetDecodingDropdowns() {
-  const dropdowns = form.querySelectorAll('[id$="decoding_L1"]', '[id$="decoding_L2"]');
-  dropdowns.forEach((dropdown) => {
-    dropdown.value = 'default';
-  });
-}
-// Function to reset all phonemic_awareness options to default
-function resetPhonemicAwarenessDropdowns() {
-  const dropdowns = form.querySelectorAll('[id$="phonemic_awareness_L1"]', '[id$="phonemic_awareness_L2"]');
-  dropdowns.forEach((dropdown) => {
-    dropdown.value = 'default';
-  });
-  resetLanguageProficiencyDropdowns();
-}
-// Function to reset all language_proficiency options to default
-function resetLanguageProficiencyDropdowns() {
-  const dropdowns = form.querySelectorAll('[id$="language_proficiency_L1"]', '[id$="language_proficiency_L2"]');
-  dropdowns.forEach((dropdown) => {
-    dropdown.value = 'default';
-  });
+  result.innerText = '';
 }
 
+function resetReadingConnectedTextDropdowns() {
+  reading_connected_text_L1.value = 'default';
+  reading_connected_text_L2.value = 'default';
+  resetReadingWordsDropdowns();
+}
+
+function resetReadingWordsDropdowns() {
+  reading_words_L1.value = 'default';
+  reading_words_L2.value = 'default';
+  resetDecodingDropdowns();
+}
+
+function resetDecodingDropdowns() {
+  decoding_L1.value = 'default';
+  decoding_L2.value = 'default';
+}
+
+// Event listener for dropdown changes
+function onLanguageProficiencyChange() {
+  result.innerText = '';
+  if (language_proficiency_L1.value !== 'default' && language_proficiency_L2.value !== 'default') {
+    reading_connected_text_panel.classList.remove('d-none');
+  } else {
+    submitButton.disabled = true;
+    reading_connected_text_panel.classList.add('d-none');
+
+  }
+}
+function onReadingConnectedTextChange() {
+  result.innerText = '';
+  if (reading_connected_text_L1.value !== 'default' && reading_connected_text_L2.value !== 'default') {
+    if (reading_connected_text_L1.value === 'not_accurate' && reading_connected_text_L2.value === 'not_accurate') {
+      submitButton.disabled = true;
+      resetReadingWordsDropdowns();
+      reading_words_panel.classList.remove('d-none');
+    }
+    else {
+      submitButton.disabled = false;
+
+      reading_words_panel.classList.add('d-none');
+      decoding_panel.classList.add('d-none');
+    }
+  } else {
+    submitButton.disabled = true;
+    reading_words_panel.classList.add('d-none');
+    decoding_panel.classList.add('d-none');
+    resetReadingWordsDropdowns();
+  }
+}
+function onReadingWordsChange() {
+  result.innerText = '';
+  if (reading_words_L1.value !== 'default' && reading_words_L2.value !== 'default') {
+    if (reading_words_L1.value === 'not_accurate' && reading_words_L2.value === 'not_accurate') {
+      submitButton.disabled = true;
+      resetDecodingDropdowns();
+      decoding_panel.classList.remove('d-none');
+    } else {
+      submitButton.disabled = false;
+    }
+  } else {
+    submitButton.disabled = true;
+    decoding_panel.classList.add('d-none');
+    resetDecodingDropdowns();
+  }
+}
+function onDecodingChange() {
+  result.innerText = '';
+  if (decoding_L1.value !== 'default' && decoding_L2.value !== 'default') {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+}
+function onPhonemicAwarenessChange() {
+  result.innerText = '';
+}
 
 // Event listener for the clear button
 clearButton.addEventListener('click', function () {
   // Reset the dropdown to the default option
   resetDropdowns();
 });
-
-/* Function to check if both dropdowns have a non-default option selected
-function isFormValid() {
-  return dropdownL1.value !== 'default' && dropdownL2.value !== 'default';
-}
-// Function to check if both dropdowns have a non-default option selected
-function isFormClearable() {
-  return dropdownL1.value !== 'default' || dropdownL2.value !== 'default';
-}
-*/
-
-
-// Event listener for dropdown changes
-function onReadingConnectedTextChange() {
-  if (reading_connected_text_L1.value !== 'default' && reading_connected_text_L2.value !== 'default') {
-    if (reading_connected_text_L1.value === 'not_accurate' && reading_connected_text_L2.value === 'not_accurate') {
-      resetReadingWordsDropdowns();
-      reading_words_panel.classList.remove('d-none');
-      resetPhonemicAwarenessDropdowns();
-      phonemic_awareness_panel.classList.add('d-none');
-      language_proficiency_panel.classList.add('d-none');
-    }
-    else {
-      phonemic_awareness_panel.classList.remove('d-none');
-      reading_words_panel.classList.add('d-none');
-      decoding_panel.classList.add('d-none');
-    }
-  } else {
-    reading_words_panel.classList.add('d-none');
-    decoding_panel.classList.add('d-none');
-    phonemic_awareness_panel.classList.add('d-none');
-    language_proficiency_panel.classList.add('d-none');
-    resetReadingWordsDropdowns();
-    resetPhonemicAwarenessDropdowns();
-  }
-}
-function onReadingWordsChange() {
-  if (reading_words_L1.value !== 'default' && reading_words_L2.value !== 'default') {
-    if (reading_words_L1.value === 'not_accurate' && reading_words_L2.value === 'not_accurate') {
-      resetDecodingDropdowns();
-      decoding_panel.classList.remove('d-none');
-      resetPhonemicAwarenessDropdowns();
-      phonemic_awareness_panel.classList.add('d-none');
-      language_proficiency_panel.classList.add('d-none');
-    }
-    else {
-      phonemic_awareness_panel.classList.remove('d-none');
-      decoding_panel.classList.add('d-none');
-    }
-  } else {
-    decoding_panel.classList.add('d-none');
-    phonemic_awareness_panel.classList.add('d-none');
-    language_proficiency_panel.classList.add('d-none');
-    resetDecodingDropdowns();
-    resetPhonemicAwarenessDropdowns();
-  }
-}
-function onDecodingChange() {
-  if (decoding_L1.value !== 'default' && decoding_L2.value !== 'default') {
-    phonemic_awareness_panel.classList.remove('d-none');
-  } else {
-    phonemic_awareness_panel.classList.add('d-none');
-    language_proficiency_panel.classList.add('d-none');
-    resetPhonemicAwarenessDropdowns();
-  }
-}
-function onPhonemicAwarenessChange() {
-  if (phonemic_awareness_L1.value !== 'default' && phonemic_awareness_L2.value !== 'default') {
-    language_proficiency_panel.classList.remove('d-none');
-  } else {
-    language_proficiency_panel.classList.add('d-none');
-    resetLanguageProficiencyDropdowns();
-  }
-}
-function onLanguageProficiencyChange() {
-  if (language_proficiency_L1.value !== 'default' && language_proficiency_L2.value !== 'default') {
-
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
-
-  }
-}
 
 
 // Event listener for dropdowns
